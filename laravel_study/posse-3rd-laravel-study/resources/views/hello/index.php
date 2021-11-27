75a53fe6528162b549e406ae7593f8af03e5b88b
taiki <dannotaiki1113@gmail.com>
2021-11-28 02:14:02 +0900

    bladeとか理解し始めた時点のpush

---
+++ laravel_study/posse-3rd-laravel-study/resources/views/hello/index.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+   <title>Hello</title>
+    
+   <style>
+   body {font: size 100pt; color:#999;}
+   h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+   </style>
+   </head>
+
+  <body>
+  <h1>Index</h1>
+ <p>This is a sample page with php-template</p>
+  </body>
+  </html>
