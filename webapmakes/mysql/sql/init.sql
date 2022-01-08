3ef4d3b41eea22f4298ab918048b55284b2cdb2f
taiki <dannotaiki1113@gmail.com>
2022-01-08 21:34:55 +0900

    test.vuerd.jsonファイルとsqlファイルを変更 -table設計 -studyテーブルはuserの手入力によってinsertさせたいため、insertを作成していない。

--- webapmakes/mysql/sql/init.sql
+++ webapmakes/mysql/sql/init.sql
@@ -1,43 +1,44 @@
-DROP SCHEMA IF EXISTS web_app;
-CREATE SCHEMA web_app;
-USE web_app;
+DROP SCHEMA IF EXISTS `web-app`;
+CREATE SCHEMA `web-app`;
+USE `web-app`;
 
--- questionsテーブル
-DROP TABLE IF EXISTS scedules;
-CREATE TABLE `scedules`(
+DROP TABLE IF EXISTS study;
+CREATE TABLE `study`(
   `id` INT NOT NULL PRIMARY KEY,
-  `info` INT NOT NULL 
-   );
-
-DROP TABLE IF EXISTS language_chart;
-
-CREATE TABLE `language_chart` (`id` INT NOT NULL PRIMARY KEY,`lang` VARCHAR(20) NOT NULL,`color` VARCHAR(20) NOT NULL);
-
-INSERT INTO `language_chart` (`id`,`lang`,`color`) VALUES
-(1, 'JavaScript', '#2A54EF'),
-(2, 'CSS', '#1B71BD'),
-(3, 'PHP', '#21BDDE'),
-(4, 'HTML', '#3DCEFD'),
-(5, 'Laravel', '#B39EF3'),
-(6, 'SQL', '#6D47EC'),
-(7, 'SHELL', '#4A18EF'),
-(8, '情報システム基礎知識（その他）', '#3107BF');
+  `study_hour` INT NOT NULL COMMENT 'study_dayにおける学習時間（時間のみ）',
+  `study_day` DATETIME COMMENT '学習をした年月日',
+  `language_id` INT NOT NULL COMMENT 'study_languageのidカラムの外部キー',
+  `content_id` INT NOT NULL COMMENT 'study_contentsのidカラムの外部キー'
+   ) COMMENT='学習に関連するデータ';
 
 
--- CREATE TABLE `contents_chart`(
---   `id` INT NOT NULL PRIMARY KEY,
---   `data` VARCHAR(20) NOT NULL,
---   `color` VARCHAR(20) NOT NULL,
---    );
+DROP TABLE IF EXISTS study_language;
+CREATE TABLE `study_language`(
+  `id` INT NOT NULL PRIMARY KEY,
+  `language`  VARCHAR(55) NOT NULL COMMENT '学習言語の名称',
+  `graph_color` VARCHAR(20) COMMENT '学習言語ごとのグラフの色'
+   ) COMMENT='学習言語に関連するデータ';
 
--- INSERT INTO `contents_chart` (`id`,`data`,`color`) VALUES
--- (1, 'ドットインストール', '#2A54EF'),
--- (2, 'N予備校', '#1B71BD'),
--- (3, 'POSSE課題', '#21BDDE'),
+INSERT INTO `study_language` (`id`,`language`,`graph_color`) VALUES
+   (1, 'JavaScript', '#2A54EF'),
+   (2, 'CSS', '#1B71BD'),
+   (3, 'PHP', '#21BDDE'),
+   (4, 'HTML', '#3DCEFD'),
+   (5, 'Laravel', '#B39EF3'),
+   (6, 'SQL', '#6D47EC'),
+   (7, 'SHELL', '#4A18EF'),
+   (8, '情報システム基礎知識（その他）', '#3107BF');
 
 
--- CREATE TABLE `bar_chart`(
---   `id` INT NOT NULL PRIMARY KEY,
---   `data` VARCHAR(20) NOT NULL,
---   `color` VARCHAR(20) NOT NULL,
---    );
+DROP TABLE IF EXISTS study_contents;
+CREATE TABLE `study_contents`(
+  `id` INT NOT NULL PRIMARY KEY,
+  `content`  VARCHAR(255) NOT NULL COMMENT '学習コンテンツの名称',
+  `graph_color` VARCHAR(20) COMMENT '学習言語ごとのグラフの色'
+   ) COMMENT='学習コンテンツに関連するデータ';
+
+INSERT INTO `study_contents` (`id`,`content`,`graph_color`) VALUES
+(1, 'ドットインストール', '#2A54EF'),
+(2, 'N予備校', '#1B71BD'),
+(3, 'POSSE課題', '#21BDDE');
+
