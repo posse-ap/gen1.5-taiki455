900f65e5af30a085a4240fc2c919fe0933f6f5fd
taiki <dannotaiki1113@gmail.com>
2022-02-08 20:43:03 +0900

    外部キー制約の設定、AUTOINCREMENTの設定、table名を複数形に修正 親テーブルのデータ削除時、子テーブルの外部キーにnullを残しておく必要がないためon delete cascadeに設定しました。

--- webapmakes/mysql/sql/init.sql
+++ webapmakes/mysql/sql/init.sql
@@ -2,24 +2,15 @@ DROP SCHEMA IF EXISTS `web-app`;
 CREATE SCHEMA `web-app`;
 USE `web-app`;
 
-DROP TABLE IF EXISTS study;
-CREATE TABLE `study`(
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `study_hour` INT NOT NULL COMMENT 'study_dayにおける学習時間（時間のみ）',
-  `study_day` DATETIME NOT NULL COMMENT '学習をした年月日',
-  `language_id` INT NOT NULL COMMENT 'study_languageのidカラムの外部キー',
-  `content_id` INT NOT NULL COMMENT 'study_contentsのidカラムの外部キー'
-   ) COMMENT='学習に関連するデータ';
 
-
-DROP TABLE IF EXISTS study_language;
-CREATE TABLE `study_language`(
+DROP TABLE IF EXISTS study_languages;
+CREATE TABLE `study_languages`(
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `language`  VARCHAR(55) NOT NULL COMMENT '学習言語の名称',
   `graph_color` VARCHAR(20) NOT NULL COMMENT '学習言語ごとのグラフの色'
    ) COMMENT='学習言語に関連するデータ';
 
-INSERT INTO `study_language` (`id`,`language`,`graph_color`) VALUES
+INSERT INTO `study_languages` (`id`,`language`,`graph_color`) VALUES
    (1, 'JavaScript', '#2A54EF'),
    (2, 'CSS', '#1B71BD'),
    (3, 'PHP', '#21BDDE'),
@@ -40,4 +31,15 @@ INSERT INTO `study_contents` (`id`,`content`,`graph_color`) VALUES
 (1, 'ドットインストール', '#2A54EF'),
 (2, 'N予備校', '#1B71BD'),
 (3, 'POSSE課題', '#21BDDE');
+
+DROP TABLE IF EXISTS studies;
+CREATE TABLE `studies`(
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `study_hour` INT NOT NULL COMMENT 'study_dayにおける学習時間（時間のみ）',
+  `study_day` DATETIME NOT NULL COMMENT '学習をした年月日',
+  `language_id` INT NOT NULL COMMENT 'study_languagesのidカラムの外部キー',
+  `content_id` INT NOT NULL COMMENT 'study_contentsのidカラムの外部キー',
+  FOREIGN KEY(`language_id`) REFERENCES `study_languages`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY(`content_id`) REFERENCES `study_contents`(`id`) ON DELETE CASCADE
+   ) COMMENT='学習に関連するデータ';
 
